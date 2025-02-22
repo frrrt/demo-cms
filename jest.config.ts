871f115a1733d323b4a44f13d846f54432bc4d4e
Moritz Thomas <mzt@live.de>
2025-02-22 14:50:21 +0100

    Added jest and setup

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+const jestConfig = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
+
+export default jestConfig;
