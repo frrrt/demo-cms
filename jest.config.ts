e3330d55d8fbb2ccfd0acba02435f0477a5336c4
Moritz Thomas <mzt@live.de>
2025-02-23 21:04:42 +0100

    Fixed jest setup

--- jest.config.ts
+++ jest.config.ts
@@ -4,13 +4,21 @@ const jestConfig = {
   testEnvironment: "jsdom",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
-  },
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(css|scss)$": "identity-obj-proxy",
   },
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          jsx: "react-jsx",
+        },
+      },
+    ],
+  },
 };
 
 export default jestConfig;
