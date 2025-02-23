60b383c552777f8a8039f0e2c5a1b49710efc6d5
Moritz Thomas <mzt@live.de>
2025-02-23 20:37:13 +0100

    Improved jest setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 const jestConfig = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
@@ -9,6 +10,7 @@ const jestConfig = {
   },
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
 
 export default jestConfig;
