44baf8ab3631c31e8bc210c8cccd4b5dd7d5bbb3
Moritz Thomas <mzt@live.de>
2025-04-17 23:07:27 +0200

    Add minimum cache time for images

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import { withPayload } from "@payloadcms/next/withPayload";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    minimumCacheTTL: 2678400,
+  },
   redirects: async () => {
     return [
       {
